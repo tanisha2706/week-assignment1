f6a53fd66d73c5387c21561c713c9286e7f8c402
tanisha2706 <99578308+tanisha2706@users.noreply.github.com>
2022-02-13 11:29:29 +0530

    added week assignment 1

---
+++ Q1.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main()
+{
+    printf("Name: Tanisha Das ");
+    printf("Reg No: 078 ");
+    printf("Branch: Electrical ");
+    printf("Hobbies: Dancing,Painting,Reading Novels,Travelling ");
+return 0;
+}
